8ddf60285037916c2e304ab910df5f1476451649
edward11900 <57603741+edward11900@users.noreply.github.com>
2022-06-08 03:44:22 +0800

    Create week14_printsFirstInAlphabeticalOrder.cpp

---
+++ week14_printsFirstInAlphabeticalOrder.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string word1, word2, word3;
+
+    cout<<"Please enter 3 words separated by a space:"<<endl;
+    cin>>word1>>word2>>word3;
+
+    if (word1 <= word2 && word1 <= word3)
+        cout<<word1<<endl;
+    else if (word2 <= word1 && word2 <= word3)
+        cout<<word2<<endl;
+    else //word3 is the smallest
+        cout<<word3<<endl;
+
+    return 0;
+
+}
