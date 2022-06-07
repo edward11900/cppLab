6f3b1c9b69697b4f40ac40c5c7be830db512813e
edward11900 <57603741+edward11900@users.noreply.github.com>
2022-06-08 03:36:37 +0800

    Update week12_array.cpp

--- week12_array.cpp
+++ week12_array.cpp
@@ -13,3 +13,24 @@ int main() {
     arr [ 4 ] = 14;
 
 }
+
+/* Basic Properties of Arrays:
+    1) The elements are stored continuously in the memory.
+
+    2) All of the elements in an array are of the same type.
+
+    3) The elements are accessed using a 0-based index system.
+
+    (address of arr[i]) = (address arr begins) + i.(Size of each element in arr)
+
+   Static array syntax:
+    1) Array's physical size must be a constant and must be given at declaration.
+
+    2) Array's name is a legal C++ expression.
+    It's value is the address in the memory where the array starts.
+
+    3) Initialization of array (at declaration)
+
+    
+
+    */
