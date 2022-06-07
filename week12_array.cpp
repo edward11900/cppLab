cd5cfadac191d7cdee75316f81b70386ce7a950c
edward11900 <57603741+edward11900@users.noreply.github.com>
2022-06-08 03:35:23 +0800

    Create week12_array.cpp

---
+++ week12_array.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    double y;
+    int arr [ 5 ];
+
+    x = 4;
+    y = 7.3;
+
+    arr [ 2 ] = 10;
+    arr [ 4 ] = 14;
+
+}
