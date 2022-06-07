4423fcb16a0effb8f7feaf49f75ee7784b2289bc
edward11900 <57603741+edward11900@users.noreply.github.com>
2022-06-08 03:20:36 +0800

    Create week6_countingProgramUsingFor.cpp

---
+++ week6_countingProgramUsingFor.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+using namespace std;
+
+int main () {
+
+    int n;
+    int counter;
+
+    cout<<"Please enter a positive integer:"<<endl;
+    cin>>n;
+
+    for (counter = 1; counter <= n; counter++) {
+        cout<<counter<<endl;
+    }
+    
+    return 0;
+}
